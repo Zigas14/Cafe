c31da83128c5c65d849661dfba52554aea89f5ec
Zigas14 <jihadfikri321@gmial.com>
2023-12-12 13:21:29 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const navbarNav = document.querySelector(".navbar-nav");
+const hamburgerMenu = document.querySelector("#hamburger-menu");
+
+hamburgerMenu.addEventListener('click', function() {
+  navbarNav.classList.toggle('active');
+}) 
+
+document.addEventListener('click', function(e) {
+   if(!hamburgerMenu.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove('active')
+   }
+})
+
+const searchForm = document.querySelector('.search-form');
+const search = document.querySelector('#search');
+const searchBox = document.querySelector("#search-box");
+search.addEventListener('click', function (e) {
+  searchForm.classList.toggle('actives');
+  searchBox.focus()
+  e.preventDefault()
+})
+
+document.addEventListener('click', function(e) {
+  if(!search.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove('actives');
+  }
+})
+
+const shoppingCart = document.querySelector('.shopping-cart');
+const cartButton = document.querySelector('#cart');
+
+cartButton.addEventListener('click', function(e) {
+ shoppingCart.classList.toggle("actives");
+ e.preventDefault()
+}) 
